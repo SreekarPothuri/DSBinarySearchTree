2abf857aeb7a408d2c537c18a1ace1ddd20c4eb5
Your Name <you@example.com>
2020-11-05 13:41:09 +0530

    Added: Test case to add Total Tree elements and get size of tree

--- src/test/java/com/blz/binarysearchtree/MyBinaryTreeTest.java
+++ src/test/java/com/blz/binarysearchtree/MyBinaryTreeTest.java
@@ -13,6 +13,27 @@ public class MyBinaryTreeTest {
 		myBinaryTree.add(70);
 		int size = myBinaryTree.getSize();
 		System.out.println(size);
-		Assert.assertEquals(4, size);
+		Assert.assertEquals(3, size);
+	}
+	
+	@Test
+	public void givenTreeStructureWhenAddedToBSTShouldReturnSize() {
+		MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<>();
+		myBinaryTree.add(56);
+		myBinaryTree.add(30);
+		myBinaryTree.add(70);
+		myBinaryTree.add(22);
+		myBinaryTree.add(40);
+		myBinaryTree.add(60);
+		myBinaryTree.add(95);
+		myBinaryTree.add(11);
+		myBinaryTree.add(65);
+		myBinaryTree.add(3);
+		myBinaryTree.add(16);
+		myBinaryTree.add(63);
+		myBinaryTree.add(67);
+		int size = myBinaryTree.getSize();
+		System.out.println(size);
+		Assert.assertEquals(13, size);
 	}
 }
