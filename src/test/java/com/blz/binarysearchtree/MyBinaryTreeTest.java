715feead6fa58f04d790536b46322532c6186a01
Your Name <you@example.com>
2020-11-05 19:41:47 +0530

    Added: Test case to search an element

--- src/test/java/com/blz/binarysearchtree/MyBinaryTreeTest.java
+++ src/test/java/com/blz/binarysearchtree/MyBinaryTreeTest.java
@@ -12,7 +12,6 @@ public class MyBinaryTreeTest {
 		myBinaryTree.add(30);
 		myBinaryTree.add(70);
 		int size = myBinaryTree.getSize();
-		System.out.println(size);
 		Assert.assertEquals(3, size);
 	}
 	
@@ -33,7 +32,26 @@ public class MyBinaryTreeTest {
 		myBinaryTree.add(63);
 		myBinaryTree.add(67);
 		int size = myBinaryTree.getSize();
-		System.out.println(size);
 		Assert.assertEquals(13, size);
+	}
+	
+	@Test
+	public void givenTreeStructureWhenAddedToBSTShouldReturnSizeAndSearchElement() {
+		MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<>();
+		myBinaryTree.add(56);
+		myBinaryTree.add(30);
+		myBinaryTree.add(70);
+		myBinaryTree.add(22);
+		myBinaryTree.add(40);
+		myBinaryTree.add(60);
+		myBinaryTree.add(95);
+		myBinaryTree.add(11);
+		myBinaryTree.add(65);
+		myBinaryTree.add(3);
+		myBinaryTree.add(16);
+		myBinaryTree.add(63);
+		myBinaryTree.add(67);
+		boolean result = myBinaryTree.searchRecursively(myBinaryTree.root,63);
+		Assert.assertTrue(true);
 	}
 }
